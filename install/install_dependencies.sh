b83107c6a4634d8fe99e46a5b23c52ae72052678
dasl- <david.leibovic@gmail.com>
2023-09-10 22:58:19 -0400

    delete unnecessary stuff

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -92,11 +92,7 @@ installNode(){
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --batch --yes --dearmor -o /etc/apt/keyrings/nodesource.gpg
 
     local NODE_MAJOR=18
-    local nodesource_list_file="/etc/apt/sources.list.d/nodesource.list"
-    local nodesource_list_text="deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main"
-    if ! grep -q "^$nodesource_list_text" $nodesource_list_file ; then
-        echo "$nodesource_list_text" | sudo tee "$nodesource_list_file"
-    fi
+    echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
     sudo apt update
     sudo apt -y install nodejs
 
