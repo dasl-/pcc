a3835a885f412d055039b4f7c9124f4f2bd516aa
David Leibovic <david.leibovic@gmail.com>
2024-11-09 16:08:41 -0500

    Update install_dependencies.sh

--- install/install_dependencies.sh
+++ install/install_dependencies.sh
@@ -98,13 +98,10 @@ installNode(){
     sudo apt -y install nodejs
 
     info "\\nInstalling react app dependencies..."
-    # TODO: when installing from scratch on a fresh OS installation, this step once failed with
-    # and error: https://gist.github.com/dasl-/01b9bf9650730c7dbfab6c859ea6c0dc
-    # See if this is reproducible on a fresh install sometime...
-    # It's weird because apparently it's a node error, but the line that is executing below is a
-    # npm command. Could npm be shelling out to node? Maybe I can figure this out by running
-    # checking the process list while the next step is running, and htop to look at RAM usage.`
-    npm install --prefix "$BASE_DIR/app"
+
+    # Limit RAM to 768 MB in attempts to avoid OOM errors: https://gist.github.com/dasl-/5abdd44806ff75f644daa9bc46b69b3e
+    # See: https://stackoverflow.com/a/40939496
+    node --max-old-space-size=786432 $(which npm) install --prefix "$BASE_DIR/app
 }
 
 fail(){
